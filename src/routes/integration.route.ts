e44dc01377bd85270946c1326fea6ee02cdf9369
Diego Reyes <diegorele13@gmail.com>
2025-10-17 13:31:41 -0500

    refactor(routes): remove unused facebook integration route

--- src/routes/integration.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { facebookConnectController } from "../controllers/integration.controller"
-
-const router = express.Router()
-
-router.post('/facebook-connect', facebookConnectController)
-
-export default router
