37a083d1fac7971e6c6b9529c0d6374fe32208e8
Diego Reyes <diegorele13@gmail.com>
2025-11-25 09:45:07 -0500

    refactor(facebook-marketing): remove unused orchestrate campaign route

--- src/routes/integrations/facebook-marketing.route.ts
+++ src/routes/integrations/facebook-marketing.route.ts
@@ -3,7 +3,6 @@ import {
   listAdAccountsController,
   saveAdAccountController,
   saveUserAccessTokenController,
-  orchestrateCampaignController,
 } from "../../controllers/integrations/facebook-marketing.controller";
 
 const router = express.Router();
@@ -11,7 +10,6 @@ const router = express.Router();
 router.get("/adaccounts/:businessId", listAdAccountsController);
 router.post("/adaccounts/:businessId/select", saveAdAccountController);
 router.post("/adaccounts/:businessId/user-token", saveUserAccessTokenController);
-router.post("/orchestrate/:businessId", orchestrateCampaignController);
 
 export default router;
 
